20fb17d4fba11747cb7ca7ac552c1494acc7f222
Liam Monahan <liam@umiacs.umd.edu>
2020-05-18 13:53:05 -0400

    Add cron update script

---
+++ cron_update.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if output=$(git --git-dir $BASEDIR/.git status --porcelain) && [ -z "$output" ];
+then
+    echo "working directory clean"
+else
+    sendmail liam@umiacs.umd.edu <<EOF
+Subject: $HOSTNAME dotfiles have uncommitted changes
+To: Liam Monahan <liam@umiacs.umd.edu>
+From: Liam Monahan <liam@umiacs.umd.edu>
+
+This host has uncommitted changes in $BASEDIR
+EOF
+fi
